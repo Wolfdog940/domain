f90b7a2ae1a7edf5310be213a742f5c4d40fe193
Oscar Urrutia <96017236+Wolfdog940@users.noreply.github.com>
2022-08-05 16:56:25 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function domain() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let domanis = "";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let texto = document.getElementById("domain");
+
+  for (var i = 0; i <= pronoun.length - 1; i++) {
+    for (var j = 0; j <= adj.length - 1; j++) {
+      for (var k = 0; k <= noun.length - 1; k++) {
+        domanis = domanis + pronoun[i] + adj[j] + noun[k] + "<br>";
+
+        texto.innerHTML = domanis;
+      }
+    }
+  }
+  return;
+}
+
+domain();
